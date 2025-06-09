9115afe33168a37d38cf700444e12648c352adae
BaltazarMolteni <baltazarmolteni04@gmail.com>
2025-06-09 16:54:15 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+
+  return (
+    <div className="bg-gradient-to-b from-purple-800 to-gray-950 min-h-screen flex justify-center">
+      <div>
+        <Header />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="relative max-w-md p-0.5 bg-gradient-to-r from-gray-300 via-white to-gray-300 ">
+            <div className=" bg-black p-8">
+                Header content
+            </div>
+        </div>
+    );
+}
+
+export default Header;
